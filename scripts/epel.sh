49ad365cc3d4445ad280582716278d4901b46ded
Francois Barriere <francois@barriere-smithson.net>
2016-06-19 12:18:23 +0200

    Split the core EPEL addition (used by all the builders) and the extra packages that are not used by all the builders.

--- scripts/epel.sh
+++ scripts/epel.sh
@@ -9,17 +9,3 @@ echo "***** Installing EPEL extras" | /usr/bin/tee -a $LOGFILE
 echo "***** Installing EPEL rpm"              | /usr/bin/tee -a $LOGFILE
 yum -y install epel-release                   | /usr/bin/tee -a $LOGFILE 2>&1
 
-echo "***** Installing extra packages"        | /usr/bin/tee -a $LOGFILE
-
-yum -y install ansible                        | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install redis                          | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install bash-completion                | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install lapack                         | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install lapack-devel                   | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install blas                           | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install blas-devel                     | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install git                            | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install cmake28                        | /usr/bin/tee -a $LOGFILE 2>&1
-
-
-
