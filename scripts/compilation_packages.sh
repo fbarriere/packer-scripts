64a91e6dcf0884b4e00a2bf7fff602b2ba204f94
Francois Barriere <francois@barriere-smithson.net>
2016-06-25 08:23:26 +0200

    Removed Ansible. Added the repositories and the devtoolset 1.1 and 2 to access gcc 4.7 and 4.8.

--- scripts/compilation_packages.sh
+++ scripts/compilation_packages.sh
@@ -6,7 +6,6 @@ LOGFILE=/root/provisioners.log
 
 echo "***** Installing extra packages"        | /usr/bin/tee -a $LOGFILE
 
-yum -y install ansible                        | /usr/bin/tee -a $LOGFILE 2>&1
 yum -y install redis                          | /usr/bin/tee -a $LOGFILE 2>&1
 yum -y install bash-completion                | /usr/bin/tee -a $LOGFILE 2>&1
 yum -y install lapack                         | /usr/bin/tee -a $LOGFILE 2>&1
@@ -16,5 +15,8 @@ yum -y install blas-devel                     | /usr/bin/tee -a $LOGFILE 2>&1
 yum -y install git                            | /usr/bin/tee -a $LOGFILE 2>&1
 yum -y install cmake28                        | /usr/bin/tee -a $LOGFILE 2>&1
 
-
+yum -y install devtoolset-1.1                 | /usr/bin/tee -a $LOGFILE 2>&1
+yum -y install devtoolset-2-perftools         | /usr/bin/tee -a $LOGFILE 2>&1
+yum -y install devtoolset-2-vc                | /usr/bin/tee -a $LOGFILE 2>&1
+yum -y install devtoolset-2-toolchain         | /usr/bin/tee -a $LOGFILE 2>&1
 
--- scripts/epel.sh
+++ scripts/epel.sh
@@ -4,8 +4,19 @@ LOGFILE=/root/provisioners.log
 
 [ -e $LOGFILE ] || /bin/touch $LOGFILE
 
-echo "***** Installing EPEL extras" | /usr/bin/tee -a $LOGFILE
+echo "***** Installing extra repositories"    | /usr/bin/tee -a $LOGFILE
 
 echo "***** Installing EPEL rpm"              | /usr/bin/tee -a $LOGFILE
 yum -y install epel-release                   | /usr/bin/tee -a $LOGFILE 2>&1
+
+echo "***** Installing devtoolset 1.1"        | /usr/bin/tee -a $LOGFILE
+cat > /etc/yum.repos.d/devtools-1.1.repo <<EOF
+[testing-1.1-devtools-\$releasever]
+name=testing 1.1 devtools for CentOS \$releasever
+baseurl=http://people.centos.org/tru/devtools-1.1/\$releasever/\$basearch/RPMS
+gpgcheck=0
+EOF
+
+echo "***** Installing devtoolset 2"          | /usr/bin/tee -a $LOGFILE
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
 
