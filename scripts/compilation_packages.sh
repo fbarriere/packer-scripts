253c8aa7bbb176548a449fb5d62fdb663f8ec812
Francois Barriere <francois@barriere-smithson.net>
2016-07-27 18:50:22 +0200

    Re-factored the devtools: moved all into the compilation_packages script (so the repositories are not added to the standard boxes).

--- scripts/compilation_packages.sh
+++ scripts/compilation_packages.sh
@@ -4,6 +4,20 @@ LOGFILE=/root/provisioners.log
 
 [ -e $LOGFILE ] || /bin/touch $LOGFILE
 
+echo "***** Installing extra repositories"    | /usr/bin/tee -a $LOGFILE
+
+echo "***** Installing devtoolset 1.1"        | /usr/bin/tee -a $LOGFILE
+cat > /etc/yum.repos.d/devtools-1.1.repo <<EOF
+[testing-1.1-devtools-\$releasever]
+name=testing 1.1 devtools for CentOS \$releasever
+baseurl=http://people.centos.org/tru/devtools-1.1/\$releasever/\$basearch/RPMS
+gpgcheck=0
+EOF
+
+echo "***** Installing devtoolset 2"          | /usr/bin/tee -a $LOGFILE
+wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
+
+
 echo "***** Installing extra packages"        | /usr/bin/tee -a $LOGFILE
 
 yum -y install redis                          | /usr/bin/tee -a $LOGFILE 2>&1
