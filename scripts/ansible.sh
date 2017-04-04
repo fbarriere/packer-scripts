bd63d650b26b1271973e08d1474a7aa59c12eeff
Francois Barriere <francois@barriere-smithson.net>
2017-04-04 16:52:09 +0200

    Merged the Python and Ansible installation into the Ansible installation script. Added the redis service enable at the end.

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,40 +1,109 @@
 #!/bin/bash
 
-PYTHON_VERSION=2.7.12
-PYTHON=/opt/python$PYTHON_VERSION
-
-VERSIONS="1.9.6 2.0.2.0-1 2.1.3.0 2.1.4.0 2.2.0.0 2.2.1.0"
-
 LOGDIR=/root
 
-echo "***** Installing extra packages"        | /usr/bin/tee -a $LOGFILE
+LOGFILE=${LOGDIR}/python_install.log
 
-yum -y install redis                          | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install sshpass                        | /usr/bin/tee -a $LOGFILE 2>&1
+# Versions section
+PYTHON_MAJOR=2.7
+PYTHON_MINOR=13
+PYTHON_VERSION=$PYTHON_MAJOR.$PYTHON_MINOR
 
+PYTHON=/opt/python$PYTHON_VERSION
 
-echo "***** Installing ansible versions"
+VERSIONS="1.9.6 2.0.2.0 2.1.5.0 2.2.2.0"
+
+ANSIBLE_EXTRAS="ansible-lint ansible-review ansible-cmdb"
+
+#
+# Python install:
+#
+
+echo "*** Installing Python $PYTHON_VERSION "
+
+TMP_PATH=~/tmp_install_python
+INSTALL_PATH=/opt/python$PYTHON_VERSION
+
+mkdir $TMP_PATH && cd $TMP_PATH
+
+# Download and extract Python and Setuptools
+
+echo "****** Downloading files."
+
+wget --no-check-certificate https://www.python.org/ftp/python/$PYTHON_VERSION/Python-$PYTHON_VERSION.tgz  > $LOGFILE 2>&1
+#wget --no-check-certificate https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
+wget --no-check-certificate https://bootstrap.pypa.io/get-pip.py  >> $LOGFILE 2>&1
+
+echo "****** Extracting Python"
+
+tar -zxf Python-$PYTHON_VERSION.tgz
+
+echo "****** Building Python"
+
+# Compile Python
+cd $TMP_PATH/Python-$PYTHON_VERSION 
+./configure --prefix=$INSTALL_PATH   >> $LOGFILE 2>&1
+make                                 >> $LOGFILE 2>&1
+make altinstall                      >> $LOGFILE 2>&1
+export PATH="$INSTALL_PATH:$PATH"
+
+# Install Setuptools and PIP
+
+echo "****** Installing SetupTools and PIP"
+cd $TMP_PATH
+#$INSTALL_PATH/bin/python$PYTHON_MAJOR ez_setup.py
+$INSTALL_PATH/bin/python$PYTHON_MAJOR get-pip.py   >> $LOGFILE 2>&1
+
+# Finish installation
+cd /
+rm -rf $TMP_PATH
+
+echo "****** Installing VirtualEnv"
+$INSTALL_PATH/bin/pip install virtualenv   >> $LOGFILE 2>&1
+
+#
+# Ansible install:
+#
+
+echo "***** Installing ansible versions"      | /usr/bin/tee -a $LOGFILE
 
 for VERSION in $VERSIONS
 do
 	LOGFILE=${LOGDIR}/ansible_${VERSION}.log
 	[ -e $LOGFILE ] || /bin/touch $LOGFILE
 	
-	echo "********** Installing version: '${VERSION}'"
-	$PYTHON/bin/virtualenv /opt/ansible-${VERSION}                          > $LOGFILE 2>&1
+	echo "********** Installing version: '${VERSION}'"                      | /usr/bin/tee -a $LOGFILE
+	$PYTHON/bin/virtualenv /opt/ansible-${VERSION}                          >> $LOGFILE
 	
-	echo "**********         Installing: pip"
-	/opt/ansible-${VERSION}/bin/easy_install pip                > $LOGFILE 2>&1
+	echo "**********         Installing: pip"                               | /usr/bin/tee -a $LOGFILE
+	/opt/ansible-${VERSION}/bin/easy_install pip                            >> $LOGFILE
 	
-	echo "**********         Upgrading: setuptools"
-	/opt/ansible-${VERSION}/bin/pip install --upgrade setuptools==11.3 > $LOGFILE 2>&1
+	echo "**********         Upgrading: setuptools"                         | /usr/bin/tee -a $LOGFILE
+	/opt/ansible-${VERSION}/bin/pip install --upgrade setuptools==11.3      >> $LOGFILE
 	
-	echo "**********         Installing: markupsafe"
-	/opt/ansible-${VERSION}/bin/pip install markupsafe          > $LOGFILE 2>&1
+	echo "**********         Installing: markupsafe"                        | /usr/bin/tee -a $LOGFILE
+	/opt/ansible-${VERSION}/bin/pip install markupsafe                      >> $LOGFILE
 	
-	echo "**********         Installing: ansible"
-	/opt/ansible-${VERSION}/bin/pip install ansible==${VERSION} > $LOGFILE 2>&1
+	echo "**********         Installing: redis"                             | /usr/bin/tee -a $LOGFILE
+	/opt/ansible-${VERSION}/bin/pip install redis                           >> $LOGFILE
 	
-	echo "**********         Installing: ansible-cmdb"
-	/opt/ansible-${VERSION}/bin/pip install ansible-cmdb
+	echo "**********         Installing: ansible"                           | /usr/bin/tee -a $LOGFILE
+	/opt/ansible-${VERSION}/bin/pip install ansible==${VERSION}             >> $LOGFILE
+	
+	for EXTRA in $ANSIBLE_EXTRAS
+	do
+		echo "**********         Installing: $EXTRA"                        | /usr/bin/tee -a $LOGFILE
+		/opt/ansible-${VERSION}/bin/pip install $EXTRA                      >> $LOGFILE
+	done
+	
 done
+
+ln -s /opt/ansible-${VERSION} /opt/ansible-latest
+
+sync
+
+echo "***** Enable redis"
+/bin/systemctl daemon-reload
+/bin/systemctl enable redis.service
+
+
