db00406c713e342dc4fa888179fe1dcd33f7781d
Francois Barriere <francois@barriere-smithson.net>
2017-12-02 10:07:23 +0100

    Separated Subversion and Doxygen installs.

---
+++ scripts/doxygen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SRC_DIR="/tmp"
+
+echo "****** Installing Doxygen."
+tar xfz ${SRC_DIR}/doxygen-1.7.6-rhel5.tar.gz -C /opt
--- scripts/svn_install.sh
+++ scripts/svn_install.sh
@@ -14,12 +14,10 @@ chmod ugo+x /opt/svn
 
 for PACKAGE in $PACKAGES
 do
-echo "Installing: ${PACKAGE}"
+echo "****** Installing: ${PACKAGE}"
 	rpm -U ${SRC_DIR}/${PACKAGE}.${ARCH}.rpm
 	cp -r /opt/CollabNet_Subversion /opt/${PACKAGE}
 	rpm -e ${PACKAGE}
 	MAJOR_MINOR=`echo ${PACKAGE} | /bin/sed -e 's/[^0-9]*\([0-9]\.[0-9]\).*/\1/'`
 	ln -s /opt/svn /opt/svn-${MAJOR_MINOR}
 done
-
-tar xfz ${SRC_DIR}/doxygen-1.7.6-rhel5.tar.gz -C /opt
