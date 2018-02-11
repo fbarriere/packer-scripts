f1ab5024c00f2c9e4ef32dfc721ca5375de929da
Francois Barriere <francois@barriere-smithson.net>
2018-02-11 21:26:58 +0100

    Install Doxygen in CentOS-6 also, so make the script OS version independant.

--- scripts/doxygen.sh
+++ scripts/doxygen.sh
@@ -2,5 +2,7 @@
 
 SRC_DIR="/tmp"
 
+OSVER=`lsb_release -rs | sed -e 's/\..*//'`
+
 echo "****** Installing Doxygen."
-tar xfz ${SRC_DIR}/doxygen-1.7.6-rhel5.tar.gz -C /opt
+tar xfz ${SRC_DIR}/doxygen-1.7.6-rhel${OSVER}.tar.gz -C /opt
