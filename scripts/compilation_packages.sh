f666022d8b7e89d090eb68ac267640d05e21c650
Francois Barriere <Francois.Barriere@microchip.com>
2021-12-02 11:28:42 +0100

    Updated the devtoolsets to 7, 8 and 9.

--- scripts/compilation_packages.sh
+++ scripts/compilation_packages.sh
@@ -1,36 +1,36 @@
-#!/bin/bash
+ #!/bin/bash
+
+TOOLSET_RELEASES="7 8 9"
 
 LOGFILE=/root/provisioners.log
 
 [ -e $LOGFILE ] || /bin/touch $LOGFILE
 
-echo "***** Installing extra repositories"    | /usr/bin/tee -a $LOGFILE
-
-echo "***** Installing devtoolset 1.1"        | /usr/bin/tee -a $LOGFILE
-cat > /etc/yum.repos.d/devtools-1.1.repo <<EOF
-[testing-1.1-devtools-\$releasever]
-name=testing 1.1 devtools for CentOS \$releasever
-baseurl=http://people.centos.org/tru/devtools-1.1/\$releasever/\$basearch/RPMS
-gpgcheck=0
-EOF
-
-echo "***** Installing devtoolset 2"          | /usr/bin/tee -a $LOGFILE
-wget http://people.centos.org/tru/devtools-2/devtools-2.repo -O /etc/yum.repos.d/devtools-2.repo
-
-
 echo "***** Installing extra packages"        | /usr/bin/tee -a $LOGFILE
 
-yum -y install redis                          | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install bash-completion                | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install lapack                         | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install lapack-devel                   | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install blas                           | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install blas-devel                     | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install git                            | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install cmake28                        | /usr/bin/tee -a $LOGFILE 2>&1
+yum -y install redis                          >> $LOGFILE 2>&1
+yum -y install bash-completion                >> $LOGFILE 2>&1
+yum -y install lapack                         >> $LOGFILE 2>&1
+yum -y install lapack-devel                   >> $LOGFILE 2>&1
+yum -y install blas                           >> $LOGFILE 2>&1
+yum -y install blas-devel                     >> $LOGFILE 2>&1
+yum -y install git                            >> $LOGFILE 2>&1
+yum -y install cmake28                        >> $LOGFILE 2>&1
 
-yum -y install devtoolset-1.1                 | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install devtoolset-2-perftools         | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install devtoolset-2-vc                | /usr/bin/tee -a $LOGFILE 2>&1
-yum -y install devtoolset-2-toolchain         | /usr/bin/tee -a $LOGFILE 2>&1
+#yum -y install devtoolset-1.1                 >> $LOGFILE 2>&1
+#yum -y install devtoolset-2-perftools         >> $LOGFILE 2>&1
+#yum -y install devtoolset-2-vc                >> $LOGFILE 2>&1
+#yum -y install devtoolset-2-toolchain         >> $LOGFILE 2>&1
+
+yum -y install gmp-devel                      >> $LOGFILE 2>&1
+yum -y install mpfr-devel                     >> $LOGFILE 2>&1
+yum -y install libmpc-devel                   >> $LOGFILE 2>&1
+
+for RELEASE in $TOOLSET_RELEASES
+do
+    echo "***** Installing devtoolset $RELEASE"                   | /usr/bin/tee -a $LOGFILE
+    yum -y install centos-release-scl                             >> $LOGFILE
+    yum-config-manager --enable rhel-server-rhscl-${RELEASE}-rpms >> $LOGFILE
+    yum -y install devtoolset-${RELEASE}                          >> $LOGFILE
+done
 
