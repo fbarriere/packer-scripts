47c2d180b1a26020c56044cf1abf106fb8e6d01b
Francois Barriere <Francois.Barriere@microchip.com>
2021-12-02 11:28:07 +0100

    Added a last and final yum update step

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,6 +4,10 @@ LOGFILE=/root/provisioners.log
 
 [ -e $LOGFILE ] || /bin/touch $LOGFILE
 
+echo "***** Running a last update *****" | /usr/bin/tee -a $LOGFILE
+
+yum -y update               | /usr/bin/tee -a $LOGFILE 2>&1
+
 echo "***** Cleaning up *****" | /usr/bin/tee -a $LOGFILE
 
 yum -y clean all                | /usr/bin/tee -a $LOGFILE 2>&1
